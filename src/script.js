7a5561ae008df0a86725b59f054d570a7907e2ed
Jalal <iamjalalhamed@gmail.com>
2022-03-02 22:59:20 +0330

    add tailwind

--- src/script.js
+++ src/script.js
@@ -1,9 +1,11 @@
 (function app() {
   const handleLoad = () => {
     setTimeout(() => {
-      document.querySelector('.pre-load').style.display = 'none';
+      // document.querySelector('.pre-load').style.display = 'none';
     }, 2000);
   };
+
+  console.log('jalal');
 
   addEventListener('load', handleLoad);
 })();
