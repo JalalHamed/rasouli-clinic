22dea4497a510d39ba8ab4b39b9d4bbbab46eb1d
Jalal <iamjalalhamed@gmail.com>
2022-03-04 16:33:08 +0330

    add preload

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function init() {
+  // Remove pre-loader
+  document.querySelector('.wrapper').style.display = 'block';
+  document.querySelector('.pre-load').style.display = 'none';
+
   // Location on map
   var map = L.map('map', {
     zoomControl: false,
@@ -6,11 +10,17 @@ function init() {
   }).setView([34.30847, 47.0629], 16);
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
   L.marker([34.30855, 47.06289]).addTo(map).openPopup();
-  setTimeout(() => {
-    map.invalidateSize();
-  }, 2000);
 
   // Replace background high-quality image once loaded
+  const bg = document.querySelector('.bg-image');
+  const image = document.createElement('img');
+  image.src = 'assets/imgs/medical.jpg';
+  image.addEventListener('load', () => {
+    bg.style.backgroundImage = `url(${image.src})`;
+    bg.style.filter = 'blur(0)';
+  });
 }
 
-window.addEventListener('load', init);
+window.addEventListener('load', () => {
+  setTimeout(init, 1000);
+});
