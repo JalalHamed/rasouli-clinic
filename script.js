4e57057145080ae74b0ec8ef83c77a790d5b0c78
Jalal <iamjalalhamed@gmail.com>
2022-03-04 16:07:06 +0330

    reduce icons and images sizes, add blured low size image until the high-quality one is loading in the background

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-var map = L.map('map', {
-  zoomControl: false,
-  attributionControl: false,
-}).setView([34.30847643759281, 47.0629013632695], 17);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-L.marker([34.308558099745525, 47.062896269031036]).addTo(map).openPopup();
+function init() {
+  // Location on map
+  var map = L.map('map', {
+    zoomControl: false,
+    attributionControl: false,
+  }).setView([34.30847, 47.0629], 16);
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+  L.marker([34.30855, 47.06289]).addTo(map).openPopup();
+  setTimeout(() => {
+    map.invalidateSize();
+  }, 2000);
+
+  // Replace background high-quality image once loaded
+}
+
+window.addEventListener('load', init);
