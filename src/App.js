00ce02a14d522e6d9b35f28ce8627c9cade80985
Jalal <iamjalalhamed@gmail.com>
2022-04-18 15:21:00 +0430

    add loading, fix not showing sections

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
+// Hooks
+import { useState, useEffect } from 'react';
 // Components
 import AppBar from './components/AppBar';
+import Loading from './components/Loading';
 // Sections
 import Home from './sections/Home';
-// Icons
-import phoneIcon from './assets/icons/phone.ico';
-import Description from './sections/Description.jsx';
+import Description from './sections/Description';
 import Treatments from './sections/Treatments';
 import Services from './sections/Services';
 import ContactUs from './sections/ContactUs';
+// Icons
+import phoneIcon from './assets/icons/phone.ico';
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1500);
+  }, []);
+
   return (
     <>
-      <div className='bg-image' />
-      <AppBar />
-      <div className='content'>
-        <Home />
-        <Description />
-        <Treatments />
-        <Services />
-        <ContactUs />
-      </div>
-      <div className='call'>
-        <a href='tel:+989031098849'>
-          <img src={phoneIcon} alt='phone' width='100%' height='100%' />
-        </a>
+      {isLoading && <Loading />}
+      <div style={{ display: isLoading ? 'none' : 'block' }}>
+        <div className='bg-image' />
+        <AppBar />
+        <div className='content'>
+          <Home />
+          <Description />
+          <Treatments />
+          <Services />
+          <ContactUs />
+        </div>
+        <div className='call'>
+          <a href='tel:+989031098849'>
+            <img src={phoneIcon} alt='phone' width='100%' height='100%' />
+          </a>
+        </div>
       </div>
     </>
   );
